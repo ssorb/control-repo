53caa60db321aa301cc1c1ecdc80b2602492e45b
ssorb <shane.sorbello@gmail.com>
2019-03-12 12:55:11 -0400

    Update db.pp

--- site/profile/manifests/app/cloudshop/webapp/db.pp
+++ site/profile/manifests/app/cloudshop/webapp/db.pp
@@ -1,6 +1,6 @@
 class profile::app::cloudshop::webapp::db (
   $dbinstance    = 'MYINSTANCE',
-  $dbpass        = lookup('profile::app::cloud shop::webapp::db::dbpass'),
+  $dbpass        = lookup('profile::app::cloudshop::webapp::db::dbpass'),
   $dbuser        = 'CloudShop',
   $dbname        = 'AdventureWorks2012',
   $dbserver      = $::hostname, # $::fqdn doesn't work on Windows / AWS
