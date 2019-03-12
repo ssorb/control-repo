edfc290eade530daf31f289caf45bfa6d8b61608
ssorb <shane.sorbello@gmail.com>
2019-03-12 14:22:42 -0400

    Update attachdb.pp

--- site/profile/manifests/app/cloudshop/sqlserver/attachdb.pp
+++ site/profile/manifests/app/cloudshop/sqlserver/attachdb.pp
@@ -7,7 +7,7 @@ define profile::app::cloudshop::sqlserver::attachdb (
   $zip_file      = 'AdventureWorks2012_Data.zip',
   $dbinstance    = 'MYINSTANCE',
   $owner         = 'CloudShop',
-  $dbpass        = 'lookup('profile::app::cloudshop::webapp::db::dbpass'),
+  $dbpass        = lookup('profile::app::cloudshop::webapp::db::dbpass'),
 ) {
   case $::profile::app::cloudshop::sqlserver::init::sqlserver_version {
     '2012':  {
