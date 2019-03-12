122a6ed7d135fbf7eff8b12ee0c15f7843e89d82
ssorb <shane.sorbello@gmail.com>
2019-03-12 12:48:42 -0400

    Update sql.pp

--- site/profile/manifests/app/cloudshop/sqlserver/sql.pp
+++ site/profile/manifests/app/cloudshop/sqlserver/sql.pp
@@ -3,7 +3,7 @@
 class profile::app::cloudshop::sqlserver::sql (
   $source      = 'F:\\',
   $db_instance = 'MYINSTANCE',
-  $sa_pass,     #= 'Password$123$',
+  $sa_pass     = lookup('profile::app::cloudshop::sqlserver::sql::sa_pass'),
 ) {
   case $profile::app::cloudshop::sqlserver::init::sqlserver_version {
     '2012':  {
