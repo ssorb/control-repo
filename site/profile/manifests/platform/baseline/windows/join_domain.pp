9a18f6fb6f141c65c08221e90f4a3eb856abddee
ssorb <shane.sorbello@gmail.com>
2019-04-08 19:14:24 -0400

    Update join_domain.pp

--- site/profile/manifests/platform/baseline/windows/join_domain.pp
+++ site/profile/manifests/platform/baseline/windows/join_domain.pp
@@ -2,9 +2,9 @@
 class profile::platform::baseline::windows::join_domain {
 
   class { 'domain_membership':
-    domain       => lookup('awskit::windows_domain::name'),
-    username     => lookup('awskit::windows_domain::join_user'),
-    password     => lookup('awskit::windows_domain::join_password'),
+    domain       => lookup('profile::platform::baseline::windows::join_domain::name'),
+    username     => lookup('profile::platform::baseline::windows::join_domain::join_user'),
+    password     => lookup('profile::platform::baseline::windows::join_domain::join_password'),
     join_options => '3',
   }
 }
