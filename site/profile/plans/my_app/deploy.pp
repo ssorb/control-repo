79b16d7108eb4ea32f00c682b4bc06496e0e6529
ssorb <shane.sorbello@gmail.com>
2019-04-09 12:10:09 -0400

    Update deploy.pp

--- site/profile/plans/my_app/deploy.pp
+++ site/profile/plans/my_app/deploy.pp
@@ -13,7 +13,7 @@ plan profile::my_app::deploy(
 
   # First query the load balancer and make sure the app isn't under too much load to do a deploy.
   unless $force {
-    $conns = run_task('profile::my_app::lb', $lb_server,
+    $conns = run_task('profile::lb', $lb_server,
        "Check load before starting deploy",
        action => 'stats',
        backend => $instance,
@@ -29,12 +29,12 @@ plan profile::my_app::deploy(
 
   # Install the new version of the application and check what version was previously
   # installed so it can be deleted after the deploy.
-  $old_versions = run_task('profile::my_app::install', [$app_servers, $db_server],
+  $old_versions = run_task('profile::install', [$app_servers, $db_server],
     "Install ${version} of the application",
     version => $version
   ).map |$r| { $r['previous_version'] }
 
-  run_task('profile::my_app::migrate', $db_server)
+  run_task('profile::migrate', $db_server)
 
   # Don't log every action on each node, only log important messages
   without_default_logging() || {
@@ -42,7 +42,7 @@ plan profile::my_app::deploy(
     get_targets($app_servers).each |$server| {
 
       # Check stats and print a message to the user
-      $stats = run_task('profile::my_app::lb', $lb_server,
+      $stats = run_task('profile::lb', $lb_server,
         action => 'stats',
         backend => $instance,
         server => $server.name,
@@ -50,7 +50,7 @@ plan profile::my_app::deploy(
       ).first
       notice("Deploying to ${server.name}, currently ${stats["status"]} with ${stats["connections"]} open connections.")
 
-      run_task('profile::my_app::lb', $lb_server,
+      run_task('profile::lb', $lb_server,
         "Drain connections from ${server.name}",
         action => 'drain',
         backend => $instance,
@@ -58,7 +58,7 @@ plan profile::my_app::deploy(
         _catch_errors => $force
       )
 
-      run_task('profile::my_app::deploy', [$server],
+      run_task('profile::deploy', [$server],
         "Update application for new version",
       )
 
